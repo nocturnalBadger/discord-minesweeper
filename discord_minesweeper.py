66da015b4068d647f9a5c0e1b4a720a4581993fe
Jeremy Schmidt <jpschmid@ncsu.edu>
2019-02-01 20:38:02 -0500

    Add initial board generation

--- discord_minesweeper.py
+++ discord_minesweeper.py
@@ -1,3 +1,4 @@
+import random
 
 emoji_digits = [":zero:", ":one:", ":two:", ":three:",
                 ":four:", ":five:", ":six:",
@@ -6,4 +7,22 @@ emoji_digits = [":zero:", ":one:", ":two:", ":three:",
 mine_emoji = ":bomb:"
 
 def generate_maze(size, num_mines):
-    bomb_pos = [random.randint(0, size) for _ in range(num_mines)
+    bomb_pos = [(random.randint(0, size), random.randint(0, size))
+            for _ in range(num_mines)]
+
+    # Initialize 2D array to all 0's
+    board = [ [0 for _ in range(size)] for _ in range(size)]
+
+    # Set "bomb" squares to -1
+    for i in range(size):
+        for j in range(size):
+            if (i, j) in bomb_pos:
+                board[i][j] = -1
+
+    print(bomb_pos)
+    print(board)
+
+
+generate_maze(5, 3)
+
+    
