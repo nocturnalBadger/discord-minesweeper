1b3984bd776605cb9041f9f1af1eada3cecec297
Jeremy Schmidt <jpschmid@ncsu.edu>
2019-02-02 20:37:42 -0500

    Update start script

--- start.sh
+++ start.sh
@@ -1,11 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-source venv/bin/activate
+if ! source venv/bin/activate; then
+    echo "Failed to activate virtual environment"
+    exit 1
+fi
 
-pip install --upgrade flask
+if ! pip install --upgrade flask; then
+    echo "Failed to install python dependencies"
+    exit 1
+fi
 
-npm install
+if ! npm install; then
+    echo "Failed to install node dependencies"
+    exit 1
+fi
 
-python discord_minesweeper.py
+python discord_minesweeper.py &
+sleep 3
 
-node bot.js
+if ! curl localhost:5000; then
+    echo "Failed to reach flask server"
+    exit 1
+fi
+
+if !  node bot.js; then
+    echo "Failed to start bot"
+    exit 1
+fi
